4337b32d9ade62d71f7408ea51f2a02de25a02c0
AlexandraZbant <alexandrazbant@gmail.com>
2023-01-04 21:53:39 +0200

    create entity with uuid primary key

--- src/main/java/com/alexandrazbant/springdatajpaintro/bootstrap/DataInitializer.java
+++ src/main/java/com/alexandrazbant/springdatajpaintro/bootstrap/DataInitializer.java
@@ -1,21 +1,25 @@
 package com.alexandrazbant.springdatajpaintro.bootstrap;
 
+import com.alexandrazbant.springdatajpaintro.domain.AuthorUuid;
 import com.alexandrazbant.springdatajpaintro.domain.Book;
+import com.alexandrazbant.springdatajpaintro.repositories.AuthorUuidRepository;
 import com.alexandrazbant.springdatajpaintro.repositories.BookRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
 @Component
 public class DataInitializer implements CommandLineRunner {
 
     private final BookRepository bookRepository;
+    private final AuthorUuidRepository authorUuidRepository;
 
-    public DataInitializer(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
+        authorUuidRepository.deleteAll();
 
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse", null);
         Book saveDDD = bookRepository.save(bookDDD);
@@ -26,8 +30,13 @@ public class DataInitializer implements CommandLineRunner {
         bookRepository.findAll().forEach(book -> {
             System.out.println("Book Id: "+ book.getId());
             System.out.println("Book Title: "+ book.getTitle());
-
         });
+
+        AuthorUuid authorUuid = new AuthorUuid();
+        authorUuid.setFirstName("Joe");
+        authorUuid.setLastName("Buck");
+        AuthorUuid savedAuthor = authorUuidRepository.save(authorUuid);
+        System.out.printf("Saved Author UUID: " + savedAuthor.getId());
 
     }
 }
