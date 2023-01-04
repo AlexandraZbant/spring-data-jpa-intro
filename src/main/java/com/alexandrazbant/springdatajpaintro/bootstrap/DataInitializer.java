650fa2838a7019f6c7589f2490bd86c498c82dca
AlexandraZbant <alexandrazbant@gmail.com>
2023-01-04 22:10:47 +0200

    create entity with binary uuid primary key

--- src/main/java/com/alexandrazbant/springdatajpaintro/bootstrap/DataInitializer.java
+++ src/main/java/com/alexandrazbant/springdatajpaintro/bootstrap/DataInitializer.java
@@ -2,8 +2,10 @@ package com.alexandrazbant.springdatajpaintro.bootstrap;
 
 import com.alexandrazbant.springdatajpaintro.domain.AuthorUuid;
 import com.alexandrazbant.springdatajpaintro.domain.Book;
+import com.alexandrazbant.springdatajpaintro.domain.BookUuid;
 import com.alexandrazbant.springdatajpaintro.repositories.AuthorUuidRepository;
 import com.alexandrazbant.springdatajpaintro.repositories.BookRepository;
+import com.alexandrazbant.springdatajpaintro.repositories.BookUuidRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,12 +16,14 @@ public class DataInitializer implements CommandLineRunner {
 
     private final BookRepository bookRepository;
     private final AuthorUuidRepository authorUuidRepository;
+    private final BookUuidRepository bookUuidRepository;
 
 
     @Override
     public void run(String... args) throws Exception {
         bookRepository.deleteAll();
         authorUuidRepository.deleteAll();
+        bookUuidRepository.deleteAll();
 
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse", null);
         Book saveDDD = bookRepository.save(bookDDD);
@@ -36,7 +40,12 @@ public class DataInitializer implements CommandLineRunner {
         authorUuid.setFirstName("Joe");
         authorUuid.setLastName("Buck");
         AuthorUuid savedAuthor = authorUuidRepository.save(authorUuid);
-        System.out.printf("Saved Author UUID: " + savedAuthor.getId());
+        System.out.println("Saved Author UUID: " + savedAuthor.getId());
+
+        BookUuid bookUuid = new BookUuid();
+        bookUuid.setTitle("All About UUIDs");
+        BookUuid savedBook = bookUuidRepository.save(bookUuid);
+        System.out.println("Saved Book UUID: " + savedBook.getId());
 
     }
 }
